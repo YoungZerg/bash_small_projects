f376c731d5e6dec57595d33cc7270d3d1a15f7c5
YoungZerg <obechekopm@gmail.com>
2026-06-07 16:21:13 +0300

    Fixed: top N counter, file path in top requested paths

--- nginx_log_analyser/nginx_log_analyser.sh
+++ nginx_log_analyser/nginx_log_analyser.sh
@@ -10,14 +10,14 @@ fi
 
 
 echo "Top ${TOP_COUNT} IP addresses with the most requests:"
-cut -d ' ' -f1 "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
+cut -d ' ' -f1 "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -n ${TOP_COUNT} | awk '{print $2 " - " $1 " requests"}'
 
 
 echo -e "\nTop ${TOP_COUNT} most requestes paths:"
-cut -d ' ' -f7 nginx-access.log | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
+cut -d ' ' -f7 "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -n ${TOP_COUNT} | awk '{print $2 " - " $1 " requests"}'
 
 echo -e "\nTop ${TOP_COUNT} most response status codes:"
-awk '{print $9}' "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
+awk '{print $9}' "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -n ${TOP_COUNT} | awk '{print $2 " - " $1 " requests"}'
 
 echo -e "\nTop ${TOP_COUNT} user agents:"
-awk -F\" '{print $6}' "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
+awk -F\" '{print $6}' "$NGINX_LOG_FILE_PATH" | sort | uniq -c | sort -rn | head -n ${TOP_COUNT} | awk '{print $2 " - " $1 " requests"}'
